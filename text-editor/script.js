ad7d0316f7ab8c85aff2627547776c07494bf18d
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-12-05 13:40:25 +0600

    Text Editor using Javascript DOM

--- text-editor/script.js
+++ text-editor/script.js
@@ -1,49 +1,39 @@
-function f1() { 
-	//function to make the text bold using DOM method 
-	document.getElementById("textarea1").style.fontWeight = "bold"; 
-} 
+function boldText() {
+    document.getElementById('textArea').style.fontWeight = "bold";
+}
 
-function f2() { 
-	//function to make the text italic using DOM method 
-	document.getElementById("textarea1").style.fontStyle = "italic"; 
-} 
+function italicText() {
+    document.getElementById('textArea').style.fontStyle = "italic";
+}
 
-function f3() { 
-	//function to make the text alignment left using DOM method 
-	document.getElementById("textarea1").style.textAlign = "left"; 
-} 
+function leftAlignText() {
+    document.getElementById('textArea').style.textAlign = "left";
+}
 
-function f4() { 
-	//function to make the text alignment center using DOM method 
-	document.getElementById("textarea1").style.textAlign = "center"; 
-} 
+function centerAlignText() {
+    document.getElementById('textArea').style.textAlign = "center";
+}
 
-function f5() { 
-	//function to make the text alignment right using DOM method 
-	document.getElementById("textarea1").style.textAlign = "right"; 
-} 
+function rightAlignText() {
+    document.getElementById('textArea').style.textAlign = "right";
+}
 
-function f6() { 
-	//function to make the text in Uppercase using DOM method 
-	document.getElementById("textarea1").style.textTransform = "uppercase"; 
-} 
+function upperText() {
+    document.getElementById('textArea').style.textTransform = "uppercase";
+}
 
-function f7() { 
-	//function to make the text in Lowercase using DOM method 
-	document.getElementById("textarea1").style.textTransform = "lowercase"; 
-} 
+function lowerText() {
+    document.getElementById('textArea').style.textTransform = "lowercase";
+}
 
-function f8() { 
-	//function to make the text capitalize using DOM method 
-	document.getElementById("textarea1").style.textTransform = "capitalize"; 
-} 
+function capitalText() {
+    document.getElementById('textArea').style.textTransform = "capitalize";
+}
 
-function f9() { 
-	//function to make the text back to normal by removing all the methods applied 
-	//using DOM method 
-	document.getElementById("textarea1").style.fontWeight = "normal"; 
-	document.getElementById("textarea1").style.textAlign = "left"; 
-	document.getElementById("textarea1").style.fontStyle = "normal"; 
-	document.getElementById("textarea1").style.textTransform = "capitalize"; 
-	document.getElementById("textarea1").value = " "; 
+function clearText() {
+    document.getElementById('textArea').style.fontWeight = 'normal';
+    document.getElementById('textArea').style.fontStyle = 'normal';
+    document.getElementById('textArea').style.textAlign = 'left';
+    document.getElementById('textArea').style.textTransform = 'capitalize';
+    document.getElementById('textArea').value = " ";
 }
